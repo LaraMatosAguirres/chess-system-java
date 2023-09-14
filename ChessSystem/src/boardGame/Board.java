4372e82f13e05244206a7e349e44d55dc58e2d9d
Lara Matos Aguirres <lara.laramatos@gmail.com>
2023-09-14 10:49:19 -0300

    Criação da classe BoardException e implementação da programação defensiva

--- ChessSystem/src/boardGame/Board.java
+++ ChessSystem/src/boardGame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece [][] pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro de criação do tabuleiro: é necessário que haja pelo menos 1 linha e 1 coluna"); 
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -21,15 +24,39 @@ public class Board {
 	}
 
 	public Piece piece (int rows, int columns) {
+		if (!positionExist(rows, columns)) {
+			throw new BoardException("Essa posição não existe no tabuleiro");
+		}
 		return pieces[rows][columns];
 	}
 	
 	public Piece piece (Position position) {
+		if (!positionExist(position)) {
+			throw new BoardException("Essa posição não existe no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPice(position)) {
+			throw new BoardException("Já exite uma peça nessa posição " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public boolean positionExist(int row, int column) {
+		return row >=0 && row< rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExist(Position position) {
+		return positionExist(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPice(Position position) {
+		if (!positionExist(position)) {
+			throw new BoardException("Essa posição não existe no tabuleiro");
+		}
+		return piece(position) != null;
 	}
 }
