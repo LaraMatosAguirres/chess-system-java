3b88f89aeca2beeefc4500d10b68e007071bd209
Lara Matos Aguirres <lara.laramatos@gmail.com>
2023-09-15 09:34:25 -0300

    Adição de método para limpar a  tela automaticamente

--- ChessSystem/src/applicacion/Program.java
+++ ChessSystem/src/applicacion/Program.java
@@ -1,7 +1,9 @@
 package applicacion;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import Chess.ChessException;
 import Chess.ChessMacth;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
@@ -14,17 +16,27 @@ public class Program {
 		ChessMacth chessmatch = new ChessMacth();
 		
 		while(true) {
-		UI.printBoard(chessmatch.getPieces());
-		System.out.println();
-		System.out.print("Digite a posição de origem: ");
-		ChessPosition source = UI.readChesssPosition(sc);
-		
-		System.out.println();
-		System.out.println("Digite a posição de destino: ");
-		ChessPosition target = UI.readChesssPosition(sc);
-		
-		ChessPiece capturedPiece = chessmatch.performChessMove(source, target);
-		
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessmatch.getPieces());
+				System.out.println();
+				System.out.print("Digite a posição de origem: ");
+				ChessPosition source = UI.readChesssPosition(sc);
+				
+				System.out.println();
+				System.out.println("Digite a posição de destino: ");
+				ChessPosition target = UI.readChesssPosition(sc);
+				
+				ChessPiece capturedPiece = chessmatch.performChessMove(source, target);
+			} catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				sc.nextLine();
+			} catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				sc.nextLine();
+			}
 		}
 	}
 
