b674eba5a360ffa7fa0d6846b0fcc76642490dee
Lara Matos Aguirres <lara.laramatos@gmail.com>
2023-09-17 15:28:15 -0300

    Programa marca no tabuleiro os possíveis movimentos  da peça

--- ChessSystem/src/applicacion/Program.java
+++ ChessSystem/src/applicacion/Program.java
@@ -23,6 +23,10 @@ public class Program {
 				System.out.print("Digite a posição de origem: ");
 				ChessPosition source = UI.readChesssPosition(sc);
 				
+				boolean[][] possibleMoves = chessmatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessmatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.println("Digite a posição de destino: ");
 				ChessPosition target = UI.readChesssPosition(sc);
